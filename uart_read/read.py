6af8be3ed0ac71f59e18c0d022d3280945e78284
raushanraja <vraushanraja@gmail.com>
2023-12-10 13:14:00 +0530

    refactor: argparse
    - Extract reading part into a method
    - Update the argpase builder

--- uart_read/read.py
+++ uart_read/read.py
@@ -2,18 +2,43 @@ import serial
 import datetime
 import argparse
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+options = {
+    'port': {
+        'name': '--port',
+        'default': '/dev/ttyUSB0',
+        'type': str,
+        'help': 'serial port, default: /dev/ttyUSB0'
+    },
+    'baud': {
+        'name': '--baud',
+        'default': 9600,
+        'type': int,
+        'help': 'baudrate, default: 9600'
+    },
+    'file': {
+        'name': '--file',
+        'default': 'temp.txt',
+        'type': str,
+        'help': 'file name, default: temp.txt'
+    }
+}
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', default='/dev/ttyUSB0', help='serial port, default: /dev/ttyUSB0')
-    parser.add_argument('--baud', default=9600, type=int, help='baudrate, default: 9600')
-    parser.add_argument('--file', default='temp.txt', help='file name, default: temp.txt')
-    args = parser.parse_args()
 
+def read_serail(args):
     ser = serial.Serial(args.port, args.baud)
-    with open('temp.txt', 'a') as file:
+    with open(args.file, 'a') as file:
         while 1:
             line = ser.readline().decode().strip()
             file.write(f'{datetime.datetime.now()},{line}\n')
             file.flush()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    for k, v in options.items():
+        name, default, type, help = v['name'], v['default'], v['type'], v['help']
+        parser.add_argument(name, default=default, type=type, help=help)
+    args = parser.parse_args()
+
+    read_serail(args)
